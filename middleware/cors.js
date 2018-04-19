17bde4a0d8417756f4ad4fcfc3502dc38ab4262c
wangruozheng <wangruozheng@zhizhangyi.com>
2018-04-19 15:52:15 +0800

    设置跨域请求

---
+++ middleware/cors.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * CORS 实现跨域访问
+ */
+module.exports = function(ctx, next) {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (ctx.method == 'OPTIONS') {
+        ctx.status = 200;
+    } else {
+        next();
+    }
+}
