73c3fda484e51214a7fd0852110b0aff5037b2a2
wangruozheng <wangruozheng@zhizhangyi.com>
2018-04-19 15:51:02 +0800

    entry js

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+
+const logger = require('./middleware/logger');
+const cors = require('./middleware/cors');
+// const route = require('./routes')
+
+const config = require('./config');
+
+const app = new Koa();
+
+app
+    .use(bodyParser())
+    .use(logger)
+    .use(cors)
+
+app.use((ctx) => {
+    ctx.body = 'asdasd'
+});
+
+// 默认端口80
+const port = config.port || 80;
+app.listen(port, () => {
+    console.log(`>>> Server is starting at port ${port}`)
+});
