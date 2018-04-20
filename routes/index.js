ed20b02e0445348c2b52e664f08489ac2dd14be0
wangruozheng <wangruozheng@zhizhangyi.com>
2018-04-20 10:26:04 +0800

    路由入口

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ const router = new Router();
 
 const login = require('./login');
 const table = require('./table');
-// const data = require('./data')
+const data = require('./data');
 
-const auth = require('../middleware/authorization')
+const auth = require('../middleware/authorization');
 
-router.use('/login', login.routes())
-router.use('/table', auth, table.routes())
-// router.use('/data', auth, data.routes())
+router.use('/login', login.routes());
+router.use('/table', auth, table.routes());
+router.use('/data', auth, data.routes());
 
 module.exports = router
