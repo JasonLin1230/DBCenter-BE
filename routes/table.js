05702fd87322ffcd3dd9725214b34f47b36ea947
wangruozheng <wangruozheng@zhizhangyi.com>
2018-04-20 10:26:19 +0800

    数据表入口

--- routes/table.js
+++ routes/table.js
@@ -6,13 +6,13 @@ const router = new Router();
 const Table = require('../controllers/table');
 const table = new Table();
 
-router.get('/', table.query.bind(table));
-
-router.get('/:tableName', table.findOne.bind(table));
-
 router.post('/', table.insert.bind(table));
 
 router.delete('/', table.delete.bind(table));
+
+router.get('/', table.query.bind(table));
+
+router.get('/:tableName', table.findOne.bind(table));
 
 
 module.exports = router
