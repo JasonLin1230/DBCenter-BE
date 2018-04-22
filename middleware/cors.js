c409e950e2d3b9656d5d933491662d6f8d667b91
wrz199306 <wangrz19936@163.com>
2018-04-22 11:54:40 +0800

    设置跨域访问

--- middleware/cors.js
+++ middleware/cors.js
@@ -6,7 +6,7 @@
 module.exports = async function(ctx, next) {
     ctx.set('Access-Control-Allow-Origin', '*');
     ctx.set('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-    ctx.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type, phone, secret');
 
     if (ctx.method == 'OPTIONS') {
         ctx.status = 200;
