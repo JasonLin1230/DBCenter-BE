3e80d2166bddf5572dbf9bb9e4ebdccbd42d4abf
wrz199306 <wangrz19936@163.com>
2018-04-22 11:54:20 +0800

    数据控制器

--- controllers/data.js
+++ controllers/data.js
@@ -9,123 +9,115 @@ module.exports = class extends Base {
 
     async insert(ctx) {
         try {
-            const table = ctx.params.table
+            const tableName = ctx.params.tableName;
     
-            const attrData = JSON.parse(ctx.request.body.attrData)
+            const attrData = JSON.parse(ctx.request.body.attrData);
         
-            const keys = Object.keys(attrData)
+            const keys = Object.keys(attrData);
         
             const values = Object.values(attrData).map((key) => {
-                return `"${key}"`
-            })
+                return `"${key}"`;
+            });
         
-            const sql = `INSERT INTO ${table} (${keys.join(',')})
+            const sql = `INSERT INTO ${tableName} (${keys.join(',')})
                             VALUES
-                                (${values.join(',')});`
+                                (${values.join(',')});`;
     
-            const result = await this.sql(sql)
-    
-            console.log('inserting success')
+            const result = await this.sql(sql);
     
             ctx.body = {
                 code: 0,
                 data: result.insertId
-            }
+            };
         } catch(err) {
             ctx.body = {
                 code: 2,
                 message: err.message
-            }
+            };
         }
     }
 
     async delete(ctx) {
         try {
-            const table = ctx.params.table
+            const table = ctx.params.tableName;
     
-            const id = ctx.request.body.id
+            const id = ctx.request.body.id;
             
-            const sql = `DELETE FROM ${table}
-                            WHERE id=${id}`
+            const sql = `DELETE FROM ${tableName}
+                            WHERE id=${id}`;
     
-            const result = await this.sql(sql)
-    
-            console.log('deleting success')
+            const result = await this.sql(sql);
     
             ctx.body = {
                 code: 0,
                 data: 'success'
-            }
+            };
         } catch(err) {
             ctx.body = {
                 code: 2,
                 message: err.message
-            }
+            };
         }
     }
 
     async update(ctx) {
         try {
-            const table = ctx.params.table
+            const tableName = ctx.params.tableName;
     
-            const { id, newAttrData } = ctx.request.body
+            const { id, newAttrData } = ctx.request.body;
     
             const resAttrData = Object.entries(JSON.parse(newAttrData)).map((attr) => {
-                return `${attr[0]}="${attr[1]}"`
-            }).join(',')
+                return `${attr[0]}="${attr[1]}"`;
+            }).join(',');
     
             const sql = `
-                UPDATE ${table}
+                UPDATE ${tableName}
                     SET ${resAttrData}
                     WHERE id=${id};
             `
     
-            const result = await this.sql(sql)
-    
-            console.log('updating success')
+            const result = await this.sql(sql);
     
             ctx.body = {
                 code: 0,
                 data: 'success'
-            }
+            };
         } catch(err) {
             ctx.body = {
                 code: 2,
                 message: err.message
-            }
+            };
         }
     }
 
     async query(ctx) {
         try {
-            const table = ctx.params.table
+            const tableName = ctx.params.tableName;
     
-            const condition = ctx.request.query.condition
+            const condition = ctx.request.query.condition;
     
-            let resCondition = ''
+            let resCondition = '';
     
             if (condition) {
                 resCondition = 'WHERE ' + Object.entries(JSON.parse(condition)).map((item) => {
-                    return `${item[0]}="${item[1]}"`
+                    return `${item[0]}="${item[1]}"`;
                 }).join(' AND ')
             }
     
-            const sql = `SELECT * from ${table} 
-                            ${resCondition};`
+            const sql = `SELECT * from ${tableName} 
+                            ${resCondition};`;
     
-            const result = await this.sql(sql)
-    
-            console.log('selecting success')
+            const result = await this.sql(sql);
     
             ctx.body = {
                 code: 0,
                 data: result
-            }
+            };
         } catch(err) {
             ctx.body = {
                 code: 2,
                 message: err.message
-            }
+            };
         }
     }
 }
